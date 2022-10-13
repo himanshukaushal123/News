2229a0b4492714ee464943d852bcbf0c39c6e9a9
R Himanshu Kuashal <55332704+himanshukaushal123@users.noreply.github.com>
2022-10-13 22:04:31 +0530

    List Update

--- src/components/List.js
+++ src/components/List.js
@@ -7,8 +7,8 @@ export const List = () => {
   };
   //loading part using usesate hook
   const [isLoading, setIsLoading] = useState(true);
-  const [data,setData]=useState([]);
-  const [url,setUrl]=useState([]);
+  const [data, setData] = useState([]);
+  const [url, setUrl] = useState([]);
 
   //fetching part
   const API = "https://hn.algolia.com/api/v1/search?query=HTML";
@@ -16,9 +16,8 @@ export const List = () => {
     try {
       const res = await fetch(url);
       const data = await res.json();
-      setData(data.hits[0].title);
-      setUrl(data.hits[0].url);
-      console.log(data.hits[0]);
+
+      setData(data.hits.slice(0, 5));
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -26,35 +25,38 @@ export const List = () => {
   };
 
   //page load on 1st time
-  useEffect(()=>{
+  useEffect(() => {
     fetchApiData(API);
-  },[])
+  }, []);
 
   //update isLoading here with use state
-  if(isLoading){
-    return(
+  if (isLoading) {
+    return (
       <>
         <h3 className="text-center">Loading...</h3>
       </>
-    )
+    );
   }
-  
+
+  console.log(data)
   return (
-    <div className="container my-3 ">
-      <div className="card bg-light" style={style}>
-        <div className="card-body bg-light">
-          <h5 className="card-title bg-light">HTML</h5>
-          <p className="card-text bg-light">
-          {data}
-          </p>
-          <a href={url} className="card-link mx-3 bg-light">
-            Read More
-          </a>
-          <a href="#" className="card-link bg-light">
-            Remove
-          </a>
+    <>
+      {data.map((item) => (
+        <div className="container my-3 ">
+          <div className="card bg-light" style={style}>
+            <div className="card-body bg-light">
+              <h5 className="card-title bg-light">HTML</h5>
+              <p className="card-text bg-light">{item.title}</p>
+              <a href={item.url} className="card-link mx-3 bg-light">
+                Read More
+              </a>
+              <a href="#" className="card-link bg-light">
+                Remove
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
